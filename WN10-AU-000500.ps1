b7271d23b9cf09e5051493f44d712c5adc7d401d
Malcolm <mwarrenn02@gmail.com>
2025-08-10 12:31:25 -0400

    Update WN10-AU-000500.ps1

--- WN10-AU-000500.ps1
+++ WN10-AU-000500.ps1
@@ -1,1 +1,36 @@
+<#
+.SYNOPSIS
+    Toggles cryptographic protocols (secure vs insecure) on the system.
+    Please test thoroughly in a non-production environment before deploying widely.
+    Make sure to run as Administrator or with appropriate privileges.
+
+.NOTES
+    Author          : Josh Madakor
+    LinkedIn        : linkedin.com/in/joshmadakor/
+    GitHub          : github.com/joshmadakor1
+    Date Created    : 2024-09-09
+    Last Modified   : 2024-09-09
+    Version         : 1.0
+    CVEs            : CVE-2014-3566
+                      CVE-2021-23839
+                      CVE-2011-3389
+    Plugin IDs      : 20007
+                      104743
+                      157288
+    STIG-ID         : WN10-CC-000105
+
+.TESTED ON
+    Date(s) Tested  : 2024-09-09
+    Tested By       : Josh Madakor
+    Systems Tested  : Windows Server 2019 Datacenter, Build 1809
+                      Windows 10 Pro, Build 22H2
+    PowerShell Ver. : 5.1.17763.6189
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\toggle-protocols.ps1 
+#>
+
+# Your code goes here
 
