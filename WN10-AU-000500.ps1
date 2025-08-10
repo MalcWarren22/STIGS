de870c029da01eab9e9aab77f8600e24ace96cf7
Malcolm <mwarrenn02@gmail.com>
2025-08-10 12:34:59 -0400

    Update WN10-AU-000500.ps1

--- WN10-AU-000500.ps1
+++ WN10-AU-000500.ps1
@@ -5,32 +5,44 @@
     Make sure to run as Administrator or with appropriate privileges.
 
 .NOTES
-    Author          : Josh Madakor
-    LinkedIn        : linkedin.com/in/joshmadakor/
-    GitHub          : github.com/joshmadakor1
-    Date Created    : 2024-09-09
-    Last Modified   : 2024-09-09
+    Author          : Malcolm Warren
+    LinkedIn        : linkedin.com/in/malcolm-warren-nsu/
+    GitHub          : github.com/MalcWarren22
+    Date Created    : 2025-08-10
+    Last Modified   : 2025-08-10
     Version         : 1.0
-    CVEs            : CVE-2014-3566
-                      CVE-2021-23839
-                      CVE-2011-3389
-    Plugin IDs      : 20007
-                      104743
-                      157288
-    STIG-ID         : WN10-CC-000105
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000500
 
 .TESTED ON
-    Date(s) Tested  : 2024-09-09
-    Tested By       : Josh Madakor
-    Systems Tested  : Windows Server 2019 Datacenter, Build 1809
-                      Windows 10 Pro, Build 22H2
+    Date(s) Tested  : 2025-08-10
+    Tested By       : Malcolm Warren
+    Systems Tested  : Windows 10 Pro, Build 22H2
     PowerShell Ver. : 5.1.17763.6189
 
 .USAGE
     Put any usage instructions here.
     Example syntax:
-    PS C:\> .\toggle-protocols.ps1 
+    PS C:\> .\STIG-ID-WN10-AU-000500.ps1 
 #>
 
-# Your code goes here
+# Define Registry Path and Value
+$logSettings = @(
+    @{ Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Application"; Name = "MaxSize"; Value = 32768 },
+    @{ Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\Security";    Name = "MaxSize"; Value = 32768 },
+    @{ Path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\EventLog\System";      Name = "MaxSize"; Value = 32768 }
+)
+
+foreach ($log in $logSettings) {
+    # Create the registry path if it doesn't exist
+    if (-not (Test-Path $log.Path)) {
+        New-Item -Path $log.Path -Force | Out-Null
+    }
+    # Set the MaxSize value
+    Set-ItemProperty -Path $log.Path -Name $log.Name -Value $log.Value -Type DWord
+    Write-Host "Set $($log.Name) to $($log.Value) KB for $($log.Path)"
+}
+
+Write-Host "Event log sizes configured for Application, Security, and System logs."
 
