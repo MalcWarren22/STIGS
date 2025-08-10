23c69d2ad8284ad8150de031adeef04e8bafde18
Malcolm <mwarrenn02@gmail.com>
2025-08-10 16:07:12 -0400

    Update and rename WN10-SO-000120 to WN10-SO-000120.ps1

---
+++ WN10-SO-000120.ps1
@@ -0,0 +1,28 @@
+<#
+.SYNOPSIS
+    Toggles cryptographic protocols (secure vs insecure) on the system.
+    Please test thoroughly in a non-production environment before deploying widely.
+    Make sure to run as Administrator or with appropriate privileges.
+
+.NOTES
+    Author          : Malcolm Warren
+    LinkedIn        : linkedin.com/in/malcolm-warren-nsu/
+    GitHub          : github.com/MalcWarren22
+    Date Created    : 2025-08-10
+    Last Modified   : 2025-08-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000120
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-10
+    Tested By       : Malcolm Warren
+    Systems Tested  : Windows 10 Pro, Build 22H2
+    PowerShell Ver. : 5.1.17763.6189
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-SO-000120/.ps1 
+#>
