b1fa1b590551337dc03d278ce83a15fcbfa070c7
Malcolm <mwarrenn02@gmail.com>
2025-08-10 18:17:39 -0400

    Update and rename WN10-SO-000150.ps1 to WN10-AU-000082 .ps1

---
+++ WN10-AU-000082 .ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that auditing for successful access to file shares is enabled
+
+.NOTES
+    Author          : Malcolm Warren
+    LinkedIn        : linkedin.com/in/malcolm-warren-nsu/
+    GitHub          : github.com/MalcWarren22
+    Date Created    : 2025-08-10
+    Last Modified   : 2025-08-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-AU-000082
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-10
+    Tested By       : Malcolm Warren
+    Systems Tested  : Windows 10 Pro, Build 22H2
+    PowerShell Ver. : 5.1.17763.6189
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-AU-000082/.ps1 
+#>
+
