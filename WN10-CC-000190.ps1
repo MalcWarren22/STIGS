a28129b9e3e6f96416a3c91c36865c14092246d7
Malcolm <mwarrenn02@gmail.com>
2025-08-10 15:11:18 -0400

    Create WN10-CC-000190.ps1

---
+++ WN10-CC-000190.ps1
@@ -0,0 +1,45 @@
+<#
+.SYNOPSIS
+    Toggles cryptographic protocols (secure vs insecure) on the system.
+    Please test thoroughly in a non-production environment before deploying widely.
+    Make sure to run as Administrator or with appropriate privileges.
+
+.NOTES
+    Author          : Malcolm Warren
+    LinkedIn        : linkedin.com/in/malcolm-warren-nsu/
+    GitHub          : github.com/MalcWarren22
+    Date Created    : 2025-08-10
+    Last Modified   : 2025-08-10
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000190
+
+.TESTED ON
+    Date(s) Tested  : 2025-08-10
+    Tested By       : Malcolm Warren
+    Systems Tested  : Windows 10 Pro, Build 22H2
+    PowerShell Ver. : 5.1.17763.6189
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> .\STIG-ID-WN10-CC-000190/.ps1 
+#>
+
+# Define AutoPlay STIG registry setting
+$autoPlaySetting = @{
+    Path  = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+    Name  = "NoDriveTypeAutoRun"
+    Value = 255  # Decimal value for 'All Drives'
+}
+
+# Create the registry path if it doesn't exist
+if (-not (Test-Path $autoPlaySetting.Path)) {
+    New-Item -Path $autoPlaySetting.Path -Force | Out-Null
+}
+
+# Set the registry value
+Set-ItemProperty -Path $autoPlaySetting.Path -Name $autoPlaySetting.Name -Value $autoPlaySetting.Value -Type DWord
+
+Write-Host "Set $($autoPlaySetting.Name) to $($autoPlaySetting.Value) at $($autoPlaySetting.Path) for 'Turn off AutoPlay' STIG compliance."
